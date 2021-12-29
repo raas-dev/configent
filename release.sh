ce7dcb12118da3e4b630de948e3f6a3d0a6fe865
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2021-12-29 15:06:22 +0200

    build(release.sh): Use Python auto-changelog and conventional commit messages

--- release.sh
+++ release.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
-pip install --user --upgrade commitizen
+# https://github.com/commitizen-tools/commitizen
+pip install --quiet --user --upgrade commitizen
+
 cz bump
 
-npx --yes auto-changelog \
-  --template keepachangelog \
-  --unreleased \
-  --hide-empty-releases \
-  --ignore-commit-pattern "^(?:[Rr]e(?:forma|inden)t|(?:[Ff]orma|[Ii]nden)t)"
+# https://github.com/KeNaCo/auto-changelog
+pip install --quiet --user --upgrade auto-changelog
+
+auto-changelog --description \
+  "All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
+and this project adheres to \
+Semantic Versioning](https://semver.org/spec/v2.0.0.html)." \
+  --starting-commit 1.2.0^1 --unreleased
 
 git add -A CHANGELOG.md
