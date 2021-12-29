d1913329545b971d4059db493fdf76755a880e24
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2021-12-29 10:56:02 +0200

    chore: Tidy up release files

--- bump.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-pip install --user --upgrade commitizen
-cz bump --changelog
--- update_changelog.sh
+++ update_changelog.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-npx -y auto-changelog --template keepachangelog \
+pip install --user --upgrade commitizen
+cz bump
+
+npx --yes auto-changelog \
+  --template keepachangelog \
   --unreleased \
   --hide-empty-releases \
-  --breaking-pattern BWIC \
-  --issue-pattern "^[Ff]ix" \
   --ignore-commit-pattern "^(?:[Rr]e(?:forma|inden)t|(?:[Ff]orma|[Ii]nden)t)"
 
 git add -A CHANGELOG.md
