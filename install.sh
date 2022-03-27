f2274591dd30ef2f70da0cb744dfd431be7b691a
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2022-03-27 18:33:57 +0300

    bump: version 1.6.1 → 1.6.2

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 ### constants  #################################################################
 
 GIT_REPO_URL="https://github.com/raas-dev/configent"
-GIT_TAG="1.6.1"
+GIT_TAG="1.6.2"
 TARGET_PATH="$HOME/configent"
 
 ### variables ##################################################################
