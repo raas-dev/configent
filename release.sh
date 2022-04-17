ac2c184f700c506373d6d1dc6fb9157ce45e0e56
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2022-04-17 12:29:29 +0300

    build(release): Add standard-version for CHANGELOG.md

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-conventional_commits_starting_tag="1.2.0"
 prerelease_type="$1"
 
 branch="$(git branch --show-current)"
@@ -14,6 +13,9 @@ if [[ -n "$(git status -s)" ]]; then
   exit 1
 fi
 
+# https://github.com/conventional-changelog/standard-version
+npx -y standard-version
+
 # https://github.com/commitizen-tools/commitizen
 pip3 install --quiet --user --upgrade commitizen
 
@@ -23,20 +25,3 @@ if [[ -n "$prerelease_type" ]]; then
 else
   "$HOME/.local/bin/cz" bump
 fi
-
-# prefer https://github.com/KeNaCo/auto-changelog over `cz changelog`
-# change target after https://github.com/KeNaCo/auto-changelog/pull/114 merged
-pip3 install --quiet --user --upgrade git+https://github.com/kapsner/auto-changelog.git
-
-"$HOME/.local/bin/auto-changelog" --description \
-  "All notable changes to this project will be documented in this file.
-
-The format is based on \
-[Keep a Changelog](https://keepachangelog.com/en/1.0.0/)
-and this project adheres to \
-[Semantic Versioning](https://semver.org/spec/v2.0.0.html)." \
-  --starting-commit "$conventional_commits_starting_tag"^1 \
-  --unreleased
-
-git add -A CHANGELOG.md
-git commit --amend --reuse-message HEAD
