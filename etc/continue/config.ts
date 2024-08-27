d4e7428c8a07097590e3d2a3454e734de8476ac3
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2024-08-27 20:54:48 +0300

    fix(continue): Configure many models of one provider

--- etc/continue/config.ts
+++ etc/continue/config.ts
@@ -5,23 +5,39 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export function modifyConfig(config: Config): Config {
-  let anthropic = config.models.find((model) => model.provider === "anthropic");
-  anthropic.apiKey = process.env.ANTHROPIC_API_KEY;
+  // Anthropic
+  config.models
+    .filter((model) => model.provider === "anthropic")
+    .forEach((anthropicModel) => {
+      anthropicModel.apiKey = process.env.ANTHROPIC_API_KEY;
+    });
 
-  config.tabAutocompleteModel.apiKey = process.env.CODESTRAL_API_KEY;
-  config.embeddingsProvider.apiKey = process.env.VOYAGE_API_KEY;
-  config.reranker.params.apiKey = process.env.VOYAGE_API_KEY;
+  // Azure OpenAI
+  config.models
+    .filter((model) => model.apiType === "azure" && model.provider === "openai")
+    .forEach((openaiModel) => {
+      openaiModel.apiBase = process.env.AZURE_OPENAI_API_BASE;
+      openaiModel.apiKey = process.env.AZURE_OPENAI_API_KEY;
+      openaiModel.apiVersion = process.env.AZURE_OPENAI_API_VERSION;
+    });
 
+  // Mistral AI
+  if (config.tabAutocompleteModel.model === "codestral-latest") {
+    config.tabAutocompleteModel.apiKey = process.env.CODESTRAL_API_KEY;
+  }
+
+  // Voyage AI
+  if (config.embeddingsProvider.model === "voyage-code-2") {
+    config.embeddingsProvider.apiKey = process.env.VOYAGE_API_KEY;
+  }
+  if (config.reranker.name === "voyage") {
+    config.reranker.params.apiKey = process.env.VOYAGE_API_KEY;
+  }
+
+  // Google search
   config.contextProviders.find(
     (provider) => provider.name === "google"
   ).params.serperApiKey = process.env.SERPER_API_KEY;
-
-  let azure_openai = config.models.find((model) => model.apiType === "azure");
-
-  azure_openai.model = process.env.AZURE_OPENAI_DEPLOYMENT_NAME;
-  azure_openai.apiBase = process.env.AZURE_OPENAI_API_BASE;
-  azure_openai.apiKey = process.env.AZURE_OPENAI_API_KEY;
-  azure_openai.apiVersion = process.env.AZURE_OPENAI_API_VERSION;
 
   return config;
 }
