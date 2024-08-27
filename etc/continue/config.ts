6c9b19ac3552ca4051283076a834e46b41cd9b17
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2024-08-27 14:36:27 +0300

    fix(continue): Add self-hosted claude and gpt-4o

--- etc/continue/config.ts
+++ etc/continue/config.ts
@@ -5,7 +5,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 export function modifyConfig(config: Config): Config {
-  config.models[0].apiKey = process.env.ANTHROPIC_API_KEY;
+  let anthropic = config.models.find((model) => model.provider === "anthropic");
+  anthropic.apiKey = process.env.ANTHROPIC_API_KEY;
+
   config.tabAutocompleteModel.apiKey = process.env.CODESTRAL_API_KEY;
   config.embeddingsProvider.apiKey = process.env.VOYAGE_API_KEY;
   config.reranker.params.apiKey = process.env.VOYAGE_API_KEY;
@@ -13,6 +15,13 @@ export function modifyConfig(config: Config): Config {
   config.contextProviders.find(
     (provider) => provider.name === "google"
   ).params.serperApiKey = process.env.SERPER_API_KEY;
+
+  let azure_openai = config.models.find((model) => model.apiType === "azure");
+
+  azure_openai.model = process.env.AZURE_OPENAI_DEPLOYMENT_NAME;
+  azure_openai.apiBase = process.env.AZURE_OPENAI_API_BASE;
+  azure_openai.apiKey = process.env.AZURE_OPENAI_API_KEY;
+  azure_openai.apiVersion = process.env.AZURE_OPENAI_API_VERSION;
 
   return config;
 }
