86546b9169b22234d33d7507716266a431ec5b5c
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2024-12-28 18:05:59 +0200

    fix(prompts): Reduce noise in add_metadata

--- etc/prompts/add_metadata.sh
+++ etc/prompts/add_metadata.sh
@@ -9,9 +9,7 @@ search_path="$1"
 
 find "$search_path" -name "*.md" -print0 | while IFS= read -r -d $'\0' file; do
   first_line=$(sed -e '/^[[:space:]]*$/d' -e '/^---$/q; d' "$file")
-  if [[ "$first_line" == "---" ]]; then
-    echo "File $file already has metadata, skipping"
-  else
+  if [[ "$first_line" != "---" ]]; then
     echo "File $file does not have metadata, adding metadata"
     {
       echo "---"
