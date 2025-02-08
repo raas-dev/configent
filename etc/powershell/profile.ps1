0b7eff3861b206ddc8bdf9c1698ba43340dd4e9f
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2025-02-08 18:06:36 +0200

    fix(pwsh): Add carapace

--- etc/powershell/profile.ps1
+++ etc/powershell/profile.ps1
@@ -1,3 +1,8 @@
 # starship
 $env:STARSHIP_CONFIG = "$HOME/.config/starship.toml"
 Invoke-Expression (&starship init powershell)
+
+# carapace
+Set-PSReadLineOption -Colors @{ "Selection" = "`e[7m" }
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+carapace _carapace | Out-String | Invoke-Expression
