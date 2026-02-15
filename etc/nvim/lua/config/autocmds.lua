1363b3e6c4550073d7d5d8652bfb76e3e3ef3f9b
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2026-02-15 14:23:01 +0200

    fix(autocmds): send osc52 yanks to clipboard
    Adds a TextYankPost autocmd that copies yanked text via osc52 when the yank operator is used.
    
    This makes yank operations update the clipboard since they previously weren’t propagated.

--- etc/nvim/lua/config/autocmds.lua
+++ etc/nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "OSC 52 copy on yank",
+  callback = function()
+    -- Only copy if it was a 'yank' operation (not a delete)
+    if vim.v.event.operator == "y" then
+      require('vim.ui.clipboard.osc52').copy('+')(vim.v.event.regcontents)
+    end
+  end,
+})
