430cd16b1cd37d436d793abb139e6a092728b01f
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2026-09-20 16:54:26 +0300

    chore(configent): cache npm/bun and disable parallel install
    - Add lima mounts for ~/.npm and ~/.bun npm registry + bun caches
    - Re-enable lima dev.yaml user script to run install.sh with background install
    - Default PARALLEL=false in install.sh to avoid install contention during reprovisioning

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ export CASKS="${CASKS:-false}"
 export FLATPAKS="${FLATPAKS:-false}"
 export SETUP_DOCKER="${SETUP_DOCKER:-true}"
 export SETUP_PODMAN="${SETUP_PODMAN:-true}"
-export PARALLEL="${PARALLEL:-true}"
+export PARALLEL="${PARALLEL:-false}"
 
 ################################################################################
 
