4ce1436337a9e6b1b6d187c017e4dd60cf466377
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2026-09-20 19:13:03 +0300

    chore: switch bunx to npx -y
    Update release.sh to use npx -y commit-and-tag-version instead of bunx.

--- release.sh
+++ release.sh
@@ -21,9 +21,9 @@ fi
 
 if [ -n "$prerelease_type" ]; then
   echo "Creating pre-release ($prerelease_type)"
-  bunx commit-and-tag-version --prerelease "$prerelease_type"
+  npx -y commit-and-tag-version --prerelease "$prerelease_type"
   uvx --from commitizen cz bump --prerelease "$prerelease_type"
 else
-  bunx commit-and-tag-version
+  npx -y commit-and-tag-version
   uvx --from commitizen cz bump
 fi
